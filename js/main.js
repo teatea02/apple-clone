be24fbb1943975ef86cc319fd0edea9049f45724
Taewook Nam <taewook122@gmail.com>
2020-09-10 18:28:27 +0900

    canvas에 image표시 기능 추가 :tada:

--- js/main.js
+++ js/main.js
@@ -16,10 +16,14 @@
                 messageA: document.querySelector('#scroll-section-0 .main-message.a'),
                 messageB: document.querySelector('#scroll-section-0 .main-message.b'),
                 messageC: document.querySelector('#scroll-section-0 .main-message.c'),
-                messageD: document.querySelector('#scroll-section-0 .main-message.d')
-
+                messageD: document.querySelector('#scroll-section-0 .main-message.d'),
+                canvas: document.querySelector('#video-canvas-0'),
+                context: document.querySelector('#video-canvas-0').getContext('2d'),
+                videoImages: []
             },
             values: {
+                videoImageCount: 300,
+                imageSequence: [0, 299],
                 messageA_opacity_in: [0, 1, { start: 0.1, end: 0.2 }],
                 messageB_opacity_in: [0, 1, { start: 0.3, end: 0.4 }],
                 messageC_opacity_in: [0, 1, { start: 0.5, end: 0.6 }],
@@ -89,6 +93,17 @@
         },
     ];
 
+    function setCanvasImages() {
+        let imgElem;
+        for (let i = 0; i < sceneInfo[0].values.videoImageCount; i++) {
+            // imgElem = document.createElement('img');
+            imgElem = new Image;
+            imgElem.src = `./images/001/IMG_${6726 + i}.JPG`;
+            sceneInfo[0].objs.videoImages.push(imgElem);
+        }
+    }
+    setCanvasImages();
+
     function setLayout() {
         // 각 스크롤 섹션의 높이 설정
         for (let i = 0; i < sceneInfo.length; i++) {
@@ -154,6 +169,8 @@
 
         switch (currentScene) {
             case 0:
+                let sequence = Math.round(calcValues(values.imageSequence, currentYOffset));
+                objs.context.drawImage(objs.videoImages[sequence], 0, 0);
                 
                 if (scrollRatio <= 0.22) {
                     // in
